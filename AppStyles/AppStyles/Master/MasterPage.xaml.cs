1bda5459cae22a4692e21dff86454159dd39ccef
Garnett05 <charlleslimamota10@hotmail.com>
2020-04-12 20:04:31 -0300

    Initial commit

---
+++ AppStyles/AppStyles/Master/MasterPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppStyles.Master
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MasterPage : MasterDetailPage
+    {
+        public MasterPage()
+        {
+            InitializeComponent();
+        }
+
+        private void GoPage (object sender, EventArgs args)
+        {
+            IsPresented = false;
+        }
+    }
+}
